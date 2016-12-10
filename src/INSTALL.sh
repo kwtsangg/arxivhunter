38b4435fb4cd2d0cd974901ce6af5a4ccd4eab9e
Ka Wa Tsang <kwtsang@nikhef.nl>
2016-12-10 03:28:55 +0100

    necessary alias to .bashrc

---
+++ src/INSTALL.sh
@@ -0,0 +1,8 @@
+# Add the following line to the .bashrc
+export ArxivDirPath="/home/kwtsang/OneDrive_CUHK/mle/arxivhunter"
+export ArxivRelSrcPath="src"
+ 
+alias arxivhunter="python ${ArxivDirPath}/${ArxivRelSrcPath}/arxivhunter.py"
+
+# Ka Wa
+# 20161210
